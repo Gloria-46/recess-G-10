3d7b3f88223917773de92a94f139e0984cdee7f7
Gloria-46 <sngloria47@gmail.com>
2025-07-25 14:12:11 +0300

    Update index.blade.php

--- UpTrend-SCM/Modules/Vendor/resources/views/orders/index.blade.php
+++ UpTrend-SCM/Modules/Vendor/resources/views/orders/index.blade.php
@@ -110,7 +110,7 @@
                     <label class="block text-sm font-medium text-gray-700 mb-2">Supplier</label>
                     <select name="supplier_id" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
                         <option value="all">All Suppliers</option>
-                        @foreach($suppliers as $supplier)
+                        @foreach($vendor_suppliers as $supplier)
                             <option value="{{ $supplier->id }}" {{ request('supplier_id') == $supplier->id ? 'selected' : '' }}>
                                 {{ $supplier->name }}
                             </option>
